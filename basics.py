2df39dc8d218d671008857877e01b879f76f1737
alunfes <ts4792@gmail.com>
2019-01-03 11:37:33 +0900

    converted name and ticket

--- basics.py
+++ basics.py
@@ -6,6 +6,17 @@ import optuna
 import string
 from sklearn import preprocessing
 import category_encoders as ce
+from sklearn.preprocessing import LabelEncoder
+
+'''
+	PassengerId	Survived	Pclass	Name	Sex	Age	SibSp	Parch	Ticket	Fare	Cabin	Embarked
+0	1	0	3	Braund, Mr. Owen Harris	male	22.0	1	0	A/5 21171	7.2500	NaN	S
+1	2	1	1	Cumings, Mrs. John Bradley (Florence Briggs Th...	female	38.0	1	0	PC 17599	71.2833	C85	C
+2	3	1	3	Heikkinen, Miss. Laina	female	26.0	0	0	STON/O2. 3101282	7.9250	NaN	S
+3	4	1	1	Futrelle, Mrs. Jacques Heath (Lily May Peel)	female	35.0	1	0	113803	53.1000	C123	S
+4	5	0	3	Allen, Mr. William Henry	male	35.0	0	0	373450	8.0500	NaN	S
+'''
+
 
 
 def readData():
@@ -38,14 +49,14 @@ def convertName(pd):
     return con
 
 
+
 def convertTicket(pd):
     t_data = pd['Ticket'].apply(lambda x: x.strip('0123456789'))
     t_data = t_data.fillna('None')
-    ce_oe = ce.OrdinalEncoder(cols=['Ticket'],handle_unknown='impute')
-    print(dir(ce_oe))
-    #df_session_ce_ordinal = ce_oe.fit_transform(df_session)
-    #df_session_ce_ordinal.head()
+    ce_oe = LabelEncoder()
+    con = ce_oe.fit_transform(t_data)
+    return con
 
 
 d = readData()
-convertTicket(d)
+print(convertTicket(d))
